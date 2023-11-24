fcd3cfed62d6f8a9ac2748c03d6321201afe6b69
khacnguyen <bbnguyenbb@live.com>
2023-11-24 13:53:45 +0700

    tạo calss Student

---
+++ OPP/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace OPP;
+internal class Student
+{
+    //các field 
+    int id;
+    string? fullname;
+    bool gender;
+    DateTime dob;
+
+    //constructor
+    public Student() { }
+
+    public Student(int id, string? fullname, bool gender, DateTime dob)
+    {
+        this.id = id;
+        this.fullname = fullname;
+        this.gender = gender;
+        this.dob = dob;
+    }
+
+
+
+}
