edac54c9d162d3703d29305c96f907468370fc38
khacnguyen <bbnguyenbb@live.com>
2023-11-24 14:41:03 +0700

    hoàn tất class

--- OPP/Student.cs
+++ OPP/Student.cs
@@ -1,23 +1,26 @@
 ﻿namespace OPP;
 internal class Student
 {
-    //các field 
-    int id;
-    string? fullname;
-    bool gender;
-    DateTime dob;
+    //vừa là field vừa là auto property
+    public int Id { get; set; }
+    public string? Fullname { get; set; }
+    public bool Gender { get; set; }
+    public DateTime Dob { get; set; }
 
     //constructor
-    public Student() { }
+    //public Student() { }
 
-    public Student(int id, string? fullname, bool gender, DateTime dob)
+    //public Student(int id, string? fullname, bool gender, DateTime dob)
+    //{
+    //    Id = id;
+    //    Fullname = fullname;
+    //    Gender = gender;
+    //    Dob = dob;
+    //}
+
+    //override tostring 
+    public override string ToString()
     {
-        this.id = id;
-        this.fullname = fullname;
-        this.gender = gender;
-        this.dob = dob;
+        return $"{nameof(Id)}={Id.ToString()}, {nameof(Fullname)}={Fullname}, {nameof(Gender)}={Gender.ToString()}, {nameof(Dob)}={Dob.ToString()}";
     }
-
-
-
 }
