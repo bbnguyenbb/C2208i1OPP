c684a8a1b4ecd493bb7b39ca6979f270cadd5338
khacnguyen <bbnguyenbb@live.com>
2023-11-29 15:00:24 +0700

    finish oop

--- InterfaceCSharp/SomeFile.cs
+++ InterfaceCSharp/SomeFile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace InterfaceCSharp;
+﻿namespace InterfaceCSharp;
 public class SomeFile : IBinaryFile, ITextFile
 {
     //thực thi các phương thức của interface
