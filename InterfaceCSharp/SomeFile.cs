03b05c0a7d1e854191ceba69e9157509f58605b6
khacnguyen <bbnguyenbb@live.com>
2023-11-29 13:19:24 +0700

    ok

---
+++ InterfaceCSharp/SomeFile.cs
@@ -0,0 +1,9 @@
+﻿namespace InterfaceCSharp;
+public class SomeFile : IBinaryFile, ITextFile
+{
+    //thực thi các phương thức của interface
+    //bổ từ truy cập (access modify) phải là public
+    internal void ReadFile() => Console.WriteLine("read file");
+    public void WriteBinaryFile() => Console.WriteLine("write binary file");
+    public void WriteTextFile() => Console.WriteLine("write text file");
+}
